af203b9a6513901b5a0321ea1ae97c204d8b70ac
David Zajicek <david.a.zajicek@outlook.com>
2023-02-10 06:17:08 +1100

    Add spell pickup items

--- bullets/ice_fist/IceFist.gd
+++ bullets/ice_fist/IceFist.gd
@@ -5,23 +5,13 @@ export var acceleration := 100.0
 onready var _animation_player := $AnimationPlayer as AnimationPlayer
 onready var _ice_splash_particles := $IceSplashParticles2D as Particles2D
 
-var current_speed : float
 
 func _ready() -> void:
 	_animation_player.connect("animation_finished", self, "_on_AnimationPlayer_animation_finished")
 	_animation_player.play("spawn")
 
-func _move(delta: float) -> void:
-	if current_speed < speed:
-		current_speed += acceleration
-	var distance := current_speed * delta
-	var motion := transform.x * current_speed * delta
-
-	position += motion
-
-	_travelled_distance += distance
-	if _travelled_distance > max_range:
-		_destroy()
+func _physics_process(delta: float) -> void:
+	speed += acceleration * delta
 
 func _destroy():
 	_disable()
